5ecd2f35f624003731402682a520cac78d49bbde
Vedant Nagawade <vedantnagawade5589@gmail.com>
2024-08-30 23:06:21 +0530

    Add files via upload

---
+++ first program.cpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int a = 5, b = 8;
+
+// Conditional operator
+
+int result = (a < b) ?b: a;
+
+cout << " The gretest number is "
+
+<< result << endl;
+
+return 0;
+
+}
